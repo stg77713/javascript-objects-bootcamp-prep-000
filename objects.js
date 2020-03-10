0acb320f23d8a062e533126374c12873744a315f
George Lugo <stg77713@gmail.com>
2020-03-10 21:32:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {'Foo Fighters': 'Everlong', Prince: 'Purple Rain' }
 
 function updatePlaylist(playlist, artist, song){
-  playlist[artist] = song;
+  playlist[artist] = song
   return playlist
 }
 
 function removePlaylist(playlist, artist){
-  delete playlist[artist];
-  return playlist;
+  delete playlist[artist]
+  return playlist
 }
