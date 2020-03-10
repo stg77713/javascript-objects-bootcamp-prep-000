4e85b2d19ace523db127a7639d26e5f2057275c4
George Lugo <stg77713@gmail.com>
2020-03-10 21:32:02 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'Foo Fighters': 'Everlong', Prince: 'Purple Rain' }
+
+function updatePlaylist(playlist, artist, song){
+  playlist.son = ;
+  return playlist
+}
+
+function removePlaylist(playlist, artist){
+  delete playlist.artist;
+  return playlist;
+}
